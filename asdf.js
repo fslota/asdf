7e072bac779355584b079a07195569c4cae89ea6
Fabian Słota <fabian.slota@gmail.com>
2018-01-25 14:34:06 +0100

    Add Jasmine for testing

--- asdf.js
+++ asdf.js
@@ -1,13 +1,29 @@
 function smallerImage() {
     document.getElementById("image1").style.width = "20%";
+    document.getElementById("image1").style.cursor = "zoom-in";
 }
 function biggerImage() {
     document.getElementById("image1").style.width = "40%";
+    document.getElementById("image1").style.cursor = "zoom-out";
 }
 function normalImage() {
     document.getElementById("image1").style.width = "30%";
+    document.getElementById("image1").style.cursor = "zoom-in";
 }
 function showTime(){
     document.getElementById("time1").innerHTML=Date().slice(0, 25);
 
+}
+function toggleImage() {
+    var x = document.getElementById('image1');
+    if (x.style.width === "40%") {
+        x.style.width = '30%';
+        x.style.cursor = "zoom-in";
+    } else {
+        x.style.width = "40%";
+        x.style.cursor = "zoom-out";
+    }
+}
+function add(x, y){
+    return x + y;
 }
---
+++ test.js
@@ -0,0 +1,9 @@
+describe("Player", function() {
+
+    it("add int numbers", function() {
+        expect(add(2,3)).toBe(5);
+    });
+    it("not wrong", function() {
+        expect(add(2,3)).toBe(7);
+    });
+});
